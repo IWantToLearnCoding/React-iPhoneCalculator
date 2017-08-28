fefab980e44221ac474358c634fcff99f03ed4bc
vineetkapoor <kapoor.vnt@gmail.com>
2017-08-27 21:42:10 -0400

    Code update

--- src/app.js
+++ src/app.js
@@ -3,63 +3,139 @@ import ReactDOM from 'react-dom'
 import './style.css'
 
 class Calculator extends React.Component {
+
     state = {
-        displayValue: '0'
+        displayValue: '0',
+        waitingForOperand: false,
+        operationToPerform: '',
+        value: 0
+    };
+
+    clearDisplay() {
+        this.setState({
+            displayValue: '0',
+            waitingForOperand: false,
+            operationToPerform: '',
+            value: 0
+        });
     }
 
     inputDigit(digit) {
-        const { displayValue } = this.state;
-        this.setState({
-            displayValue: displayValue === '0' ? String(digit) : displayValue+digit
-        })
+        const { displayValue, waitingForOperand } = this.state;
+
+        if(waitingForOperand) {
+            this.setState({
+                value: displayValue,
+                displayValue: String(digit),
+                waitingForOperand: false
+            })
+        } else {
+            this.setState({
+                displayValue: displayValue === '0' ? String(digit) : displayValue+String(digit)
+            })
+        }
     }
 
     inputDot() {
-        const { displayValue } = this.state;
-        if(displayValue.indexOf('.') === -1) {
+        const { displayValue, waitingForOperand } = this.state;
+
+        if(waitingForOperand) {
+            this.setState({
+                value: displayValue,
+                displayValue: '.',
+                waitingForOperand: false
+            })
+        } else if(displayValue.indexOf('.') === -1) {
             this.setState({
                 displayValue: displayValue + '.'
             })
         }
     }
 
-  render() {
-      const { displayValue } = this.state;
-      return (
-          <div className="calculator">
-            <div className="calculator-display">{displayValue}</div>
-            <div className="calculator-keypad">
-                <div className="input-keys">
-                    <div className="function-keys">
-                    <button className="calculator-key key-clear">AC</button>
-                    <button className="calculator-key key-sign">±</button>
-                    <button className="calculator-key key-percent">%</button>
+    toggleSign() {
+        let { displayValue } = this.state;
+        if(displayValue !== '0') {
+            displayValue =  displayValue.charAt(0) === '-' ? displayValue.substr(1) : '-' + displayValue;
+        }
+
+        this.setState({
+            displayValue: displayValue
+        });
+    }
+
+    calculatePercent() {
+        let { displayValue } = this.state;
+        if(displayValue !== '0') {
+            displayValue =  String(parseFloat(displayValue) / 100);
+        }
+        this.setState({
+            displayValue: displayValue
+        })
+    }
+
+    performOperation(operator) {
+        const { displayValue, operationToPerform, value } = this.state;
+        const operations = {
+            '/' : (prevOperand, currentOperand) => Number(prevOperand)/ Number(currentOperand),
+            '*' : (prevOperand, currentOperand) => Number(prevOperand) * Number(currentOperand),
+            '-' : (prevOperand, currentOperand) => Number(prevOperand) - Number(currentOperand),
+            '+' : (prevOperand, currentOperand) => Number(prevOperand) + Number(currentOperand),
+            '=' : (prevOperand, currentOperand) => Number(prevOperand),
+        }
+
+        if(operationToPerform) {
+            const calculatedValue = String(operations[operationToPerform](value, displayValue))
+            this.setState({
+                displayValue: calculatedValue,
+                waitingForOperand: true,
+                operationToPerform: operator,
+                value: calculatedValue
+            })
+        } else {
+            this.setState({
+                waitingForOperand: true,
+                operationToPerform: operator
+            });
+        }
+    }
+
+    render() {
+        const { displayValue } = this.state;
+        return (
+              <div className="calculator">
+                <div className="calculator-display">{displayValue}</div>
+                <div className="calculator-keypad">
+                    <div className="input-keys">
+                        <div className="function-keys">
+                        <button className="calculator-key key-clear" onClick={ () => this.clearDisplay() }>AC</button>
+                        <button className="calculator-key key-sign" onClick={ () => this.toggleSign() }>±</button>
+                        <button className="calculator-key key-percent" onClick={ () => this.calculatePercent() }>%</button>
+                    </div>
+                    <div className="digit-keys">
+                        <button className="calculator-key key-0" onClick={ () => this.inputDigit(0) }>0</button>
+                        <button className="calculator-key key-dot" onClick={ () => this.inputDot() }>●</button>
+                        <button className="calculator-key key-1" onClick={ () => this.inputDigit(1) }>1</button>
+                        <button className="calculator-key key-2" onClick={ () => this.inputDigit(2) }>2</button>
+                        <button className="calculator-key key-3" onClick={ () => this.inputDigit(3) }>3</button>
+                        <button className="calculator-key key-4" onClick={ () => this.inputDigit(4) }>4</button>
+                        <button className="calculator-key key-5" onClick={ () => this.inputDigit(5) }>5</button>
+                        <button className="calculator-key key-6" onClick={ () => this.inputDigit(6) }>6</button>
+                        <button className="calculator-key key-7" onClick={ () => this.inputDigit(7) }>7</button>
+                        <button className="calculator-key key-8" onClick={ () => this.inputDigit(8) }>8</button>
+                        <button className="calculator-key key-9" onClick={ () => this.inputDigit(9) }>9</button>
+                    </div>
+              </div>
+              <div className="operator-keys">
+                    <button className="calculator-key key-divide" onClick={ () => this.performOperation('/') }>÷</button>
+                    <button className="calculator-key key-multiply" onClick={ () => this.performOperation('*') }>×</button>
+                    <button className="calculator-key key-subtract" onClick={ () => this.performOperation('-') }>−</button>
+                    <button className="calculator-key key-add" onClick={ () => this.performOperation('+') }>+</button>
+                    <button className="calculator-key key-equals" onClick={ () => this.performOperation('=') }>=</button>
                 </div>
-                <div className="digit-keys">
-                    <button className="calculator-key key-0" onClick={ ()=>this.inputDigit(0) }>0</button>
-                    <button className="calculator-key key-dot" onClick={ ()=>this.inputDot() }>●</button>
-                    <button className="calculator-key key-1" onClick={ ()=>this.inputDigit(1) }>1</button>
-                    <button className="calculator-key key-2" onClick={ ()=>this.inputDigit(2) }>2</button>
-                    <button className="calculator-key key-3" onClick={ ()=>this.inputDigit(3) }>3</button>
-                    <button className="calculator-key key-4" onClick={ ()=>this.inputDigit(4) }>4</button>
-                    <button className="calculator-key key-5" onClick={ ()=>this.inputDigit(5) }>5</button>
-                    <button className="calculator-key key-6" onClick={ ()=>this.inputDigit(6) }>6</button>
-                    <button className="calculator-key key-7" onClick={ ()=>this.inputDigit(7) }>7</button>
-                    <button className="calculator-key key-8" onClick={ ()=>this.inputDigit(8) }>8</button>
-                    <button className="calculator-key key-9" onClick={ ()=>this.inputDigit(9) }>9</button>
-                </div>
-          </div>
-          <div className="operator-keys">
-                <button className="calculator-key key-divide">÷</button>
-                <button className="calculator-key key-multiply">×</button>
-                <button className="calculator-key key-subtract">−</button>
-                <button className="calculator-key key-add">+</button>
-                <button className="calculator-key key-equals">=</button>
             </div>
-        </div>
-      </div>
-    )
-  }
+          </div>
+        )
+    }
 }
 
 ReactDOM.render(
